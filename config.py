5069fcb3fdf52ac3912b6f2e0fb0f403da342304
stavridka9 <121058165+stavridka@users.noreply.github.com>
2024-03-29 22:18:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters.callback_data import CallbackData
-
 
 Bot_token = '5933923237:AAEjRD4ZWRq-4tUD3OoUiIV-ojrswsm-xHA'
 test_key = '-4160869434'
 GROUP_ID = -1001580602852
 ADMIN_ID = [313157090, 694086094,1185602840]
+tab_names = ['DB_image1.txt','DB_image2.txt']
+
 names = """Агапов Арсений Александрович
 Бальва Илья Владиславович
 Бедрышева Мария Сергеевна
@@ -37,12 +37,12 @@ names = """Агапов Арсений Александрович
 Ушков Андрей Валерьевич
 Халил Тауфик Абдулла""".split("\n")
 
+
+
+
 surnames = []
 for name in names:
     surnames.append(name.split()[0])
 
-
 bot = Bot(token=Bot_token)
 
-class MyCallBack(CallbackData,prefix='my'):
-    call:str
